50f6dd2b4b637753380ae8397601b40c8a55bfce
vlacho13 <striuncekf@hotmail.com>
2018-11-10 23:55:29 -0500

    implemented render_template and url_for functions with bootstrap snippets

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Vlad Luna',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'November 10, 2018'
+    },
+    {
+        'author': 'Gabe Luna',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'November 10, 2018'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 
