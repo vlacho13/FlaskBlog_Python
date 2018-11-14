8244f9648464a35751504ffcfd72a614627ea4e5
vlacho13 <striuncekf@hotmail.com>
2018-11-14 01:04:34 -0500

    added forms classes for login and registration and register template

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=10)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=4, max=10)])
+    confirm_password = PasswordField("Password", validators=[DataRequired(), EqualTo("password"), Length(min=4, max=10)])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=4, max=10)])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
+    
+
