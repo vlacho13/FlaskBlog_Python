56fb683a8380ba7ec56cb72aa6c78d986722c86a
vlacho13 <striuncekf@hotmail.com>
2018-11-20 23:14:37 -0500

    imported flask_mail, users can now request a password reset, generating a token for 30mins

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -53,5 +53,18 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email is None:
+            raise ValidationError('There is no account associated with that email. Please register first')
     
 
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=4, max=10)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password"), Length(min=4, max=10)])
+    submit = SubmitField("Reset Password")
